870851833bd16faf23317124d96b3c941149cbe2
Ana <anabalbi98@gmail.com>
2022-07-16 21:32:17 -0300

    urls

--- ProyectoCoder/urls.py
+++ ProyectoCoder/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('clientes/', include('Clientes.urls'), name='Clientes'),
     path('artistas/', include('Artistas.urls'), name='Artistas'),
+    path('discos/', include('Discos.urls'), name='Discos'),
 ]
