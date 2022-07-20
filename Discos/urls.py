94b0a033b196f80bbc3a4e1513092e7dbdc65e21
Ana <anabalbi98@gmail.com>
2022-07-20 18:27:16 -0300

    arreglo algunos errores

--- Discos/urls.py
+++ Discos/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 from Discos.views import *
 
 
-urlpattern= [
-    path('Discos/', render_view_discos, name=Discos),
+urlpatterns = [
+    path('Discos/', render_view_discos, name="ver_discos"),
     path('formulario_disco/', formulario_disco),
     path('busquedadiscos/', busqueda_discos),
-    path('buscar/', buscar)
-
-
+    path('buscar/', buscar),
+    path('listadiscos/', Discoslist.as_view(),name='discos_lista'),
 ]
--- Discos/views.py
+++ Discos/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from Discos.models import Discos, Genero
 from django.http import HttpResponse
-from discos_forms import Discosformularios
+from .discos_forms import Discosformularios
 from django.views.generic import ListView
 from django.views.generic import DetailView
 
