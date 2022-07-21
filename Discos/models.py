6b6b5e6b6246b24b5bd72296a3699a2b4b98dde9
Andres Convertini <andres.convertini91@gmail.com>
2022-07-21 20:57:20 -0300

    Se agrega clase Canciones

--- Discos/models.py
+++ Discos/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib import admin
+from Artistas.models import Artista, Banda
 from Base.models import Base
 
 # Create your models here.
@@ -11,6 +12,25 @@ class Discos(Base):
 
 class Genero(Base):
     nombre=models.CharField(max_length=50)
+
+class Canciones(Base):
+    nombre = models.CharField(max_length=50)
+    lanzamiento = models.DateField(null=None)
+    discos = models.ForeignKey(Discos, on_delete=models.PROTECT, null=False)
+    bandas = models.ManyToManyField(
+        Banda,
+        through='Canciones_Banda'
+        )
+
+    def __str__(self):
+        return self.nombre
     
+class Canciones_Banda(Base):
+    cancion = models.ForeignKey(
+        Canciones, on_delete=models.CASCADE, blank=True, null=False
+    )
+    banda = models.ForeignKey(
+        Banda, on_delete=models.CASCADE, blank=True, null=False
+    )
 
 admin.site.register([Genero, Discos])
